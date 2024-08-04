7e603f619ad74003cf797f7516f1610a8c2214df
Lucas <lucasoom3@hotmail.com>
2024-08-04 19:47:52 -0300

    desafio1.0

--- src/main.py
+++ src/main.py
@@ -2,47 +2,87 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Carregar os dados
-data_path = 'data/netflixdata.csv'
+data_path = 'data/dataset_olympics.csv'
 df = pd.read_csv(data_path)
 
-# Função para carregar dados
+cor_medalha = ['#636EFA']
+cor_sexo = ['#EF553B']
+cor_idade = ['#00CC96']
+cor_peso = ['#AB63FA']
+cor_time = ['#FFA15A']
+cor_jogos = ['#19D3F3']
+cor_total_medalhas = ['#FF6692']
+
 def load_data():
     df = pd.read_csv(data_path)
     return df
 
-# Título e descrição do Dashboard
-st.title('Dashboard Netflix')
-st.markdown('## Análise dos Dados de Filmes e Séries da Netflix')
+st.title('Dashboard Olímpico')
+st.markdown('## Análise dos Dados de Atletas Olímpicos')
 st.sidebar.image('images/logo.png', width=150)
-st.sidebar.markdown('**Aluno:** Lucas Albquerque Santos Costa')
+st.sidebar.markdown('**Aluno:** Lucas Albuquerque Santos Costa')
 st.sidebar.markdown('**PDITA:** PDBD009')
 
-# Widgets de Interatividade
-type_filter = st.sidebar.selectbox('Selecione o Tipo', df['type'].unique())
-country_filter = st.sidebar.selectbox('Selecione o País', df['country'].dropna().unique())
+season_filter = st.sidebar.selectbox('Selecione a Temporada', df['Season'].unique())
+country_filter = st.sidebar.selectbox('Selecione o País', df['Team'].dropna().unique())
+filtered_data = df[(df['Season'] == season_filter) & (df['Team'] == country_filter)]
 
-# Filtrar dados com base na seleção
-filtered_data = df[(df['type'] == type_filter) & (df['country'] == country_filter)]
-
-# Gráfico de Barras: Contagem de Títulos por Ano de Lançamento
-grouped_data = filtered_data.groupby('release_year').size().reset_index(name='count')
-fig = px.bar(grouped_data, x='release_year', y='count', title=f'Títulos por Ano de Lançamento ({type_filter} - {country_filter})', labels={'count': 'Contagem de Títulos', 'release_year': 'Ano de Lançamento'})
+#Contagem de medalhas
+medal_data = filtered_data['Medal'].value_counts().reset_index()
+medal_data.columns = ['Medal', 'Count']
+fig = px.bar(medal_data, x='Medal', y='Count', title=f'Contagem de Medalhas ({season_filter} - {country_filter})', labels={'Count': 'Contagem de Medalhas', 'Medal': 'Medalha'},color_discrete_sequence= cor_medalha)
 st.plotly_chart(fig)
 
-# Gráfico de Pizza: Distribuição das Durações dos Filmes
-# Filtrar dados apenas para filmes (já que séries têm duração em temporadas)
-if type_filter == 'Movie':
-    duration_data = filtered_data['duration'].value_counts().reset_index()
-    duration_data.columns = ['duration', 'count']
-    fig_pie = px.pie(duration_data, values='count', names='duration', title='Distribuição das Durações dos Filmes')
-    st.plotly_chart(fig_pie)
+# Alturas atletas
+height_data = filtered_data['Height'].value_counts().reset_index()
+height_data.columns = ['Height', 'Count']
+fig_pie = px.pie(height_data, values='Count', names='Height', title='Distribuição das Alturas dos Atletas')
+st.plotly_chart(fig_pie)
 
-# Exibir Tabela de Dados
-st.markdown(f'## Dados Filtrados para {type_filter} em {country_filter}')
+#  atletas por Sexo
+sex_data = filtered_data['Sex'].value_counts().reset_index()
+sex_data.columns = ['Sex', 'Count']
+fig_sex = px.bar(sex_data, x='Sex', y='Count', title=f'Contagem de Atletas por Sexo ({season_filter} - {country_filter})', labels={'Count': 'Contagem de Atletas', 'Sex': 'Sexo'},color_discrete_sequence=cor_sexo)
+st.plotly_chart(fig_sex)
+
+# Idade
+fig_age = px.line(filtered_data, x='ID', y='Age', title=f'Idades dos Atletas ({season_filter} - {country_filter})', labels={'ID': 'ID do Atleta', 'Age': 'Idade'},color_discrete_sequence=cor_idade)
+st.plotly_chart(fig_age)
+
+# Peso
+weight_data = filtered_data['Weight'].value_counts().reset_index()
+weight_data.columns = ['Weight', 'Count']
+fig_weight = px.bar(weight_data, x='Weight', y='Count', title='Distribuição dos Pesos dos Atletas', labels={'Count': 'Contagem', 'Weight': 'Peso'},color_discrete_sequence= cor_peso)
+st.plotly_chart(fig_weight)
+
+# Numero de atletas
+team_data = filtered_data['Team'].value_counts().reset_index()
+team_data.columns = ['Team', 'Count']
+fig_team = px.bar(team_data, x='Team', y='Count', title='Contagem de Atletas por Time', labels={'Count': 'Contagem', 'Team': 'Time'}, color_discrete_sequence=cor_time)
+st.plotly_chart(fig_team)
+
+# Numero por jogos
+games_data = filtered_data['Games'].value_counts().reset_index()
+games_data.columns = ['Games', 'Count']
+fig_games = px.bar(games_data, x='Games', y='Count', title='Contagem de Atletas por Jogos', labels={'Count': 'Contagem', 'Games': 'Jogos'}, color_discrete_sequence=cor_jogos)
+st.plotly_chart(fig_games)
+
+# medalhas de cada tipo.
+medal_data = filtered_data['Medal'].value_counts().reset_index()
+medal_data.columns = ['Medal', 'Count']
+fig_medal = px.bar(medal_data, x='Medal', y='Count', title='Contagem de Medalhas por Tipo', labels={'Count': 'Contagem', 'Medal': 'Medalha'}, color_discrete_sequence=cor_medalha)
+st.plotly_chart(fig_medal)
+
+# medalhas por pais
+total_medals = df[df['Medal'].notna()]
+total_medals = total_medals.groupby('Team').size().reset_index(name='Count')
+fig_total_medals = px.bar(total_medals, x='Team', y='Count', title='Total de Medalhas por País', labels={'Count': 'Contagem de Medalhas', 'Team': 'País'}, color_discrete_sequence=cor_total_medalhas)
+st.plotly_chart(fig_total_medals)
+
+# tabela de Dados
+st.markdown(f'## Dados Filtrados para {season_filter} em {country_filter}')
 st.write(filtered_data)
 
-# CSS para estilização
 st.markdown(
     """
     <style>
